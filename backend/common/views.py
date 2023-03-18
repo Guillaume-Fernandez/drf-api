9c9c55ddce9e47de829d50fee41d5ca69432dfcc
Guillaume <60733426+Guillaume-Fernandez@users.noreply.github.com>
2023-03-18 11:26:41 +0000

    Rewrit common views

--- backend/common/views.py
+++ backend/common/views.py
@@ -1,28 +1,25 @@
 from rest_framework import viewsets, renderers, views, response, authentication, permissions
 from rest_framework.pagination import PageNumberPagination
-import django.urls.resolvers
 
-from k8s.utils import list_ns_with_status
-import labs.urls  
-from .renderers import LabsRenderer
-from .permissions import LabsUserPermission
+from .renderers import CommonRenderer
+from .permissions import CommonUserPermission
 
 class LabsResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
-class LabsBaseViewSet(LabsUserPermission, viewsets.ModelViewSet):
-    renderer_classes = [LabsRenderer, renderers.BrowsableAPIRenderer, renderers.JSONRenderer]
+class LabsBaseViewSet(CommonUserPermission, viewsets.ModelViewSet):
+    renderer_classes = [CommonRenderer, renderers.BrowsableAPIRenderer, renderers.JSONRenderer]
     pagination_class = LabsResultsSetPagination
     # permission_classes = [LabsUserPermission]
     # authentication_classes = [authentication.SessionAuthentication]
 
 
-class DashboardAPIView(LabsUserPermission, views.APIView):
-    name = 'Labs'
+class DashboardAPIView(CommonUserPermission, views.APIView):
+    name = 'SampleApp'
     renderer_classes = [renderers.TemplateHTMLRenderer, renderers.BrowsableAPIRenderer, renderers.JSONRenderer]
-    template_name = 'labs_common/dashboard.html'
+    template_name = 'common/dashboard.html'
     authentication_classes = [authentication.SessionAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
@@ -33,9 +30,7 @@ class DashboardAPIView(LabsUserPermission, views.APIView):
         API/JSON => Shorter description and infrastructure status.
         """
 
-        environments, nb_envs_running = list_ns_with_status()
         return response.Response({
-            'hi': 'Welcome at labs.open-prod.com. The labs is a project aiming to automate recurring system tasks.',
-            'environments': environments,
-            'nb_envs_running': nb_envs_running,
+            'hi': 'Welcome at my-website.com.',
+            'varA': 'You may want to print global information there',
         })
