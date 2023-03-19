d6002396bf476e83ff1e170562de50c09858c7c9
Guillaume Fernandez <guillaume.fernandez@open-prod.com>
2023-03-19 20:53:48 +0100

    Add default ordering

--- backend/demo/views.py
+++ backend/demo/views.py
@@ -1,3 +1,5 @@
+from rest_framework import filters
+
 from common.views import CommonBaseViewSet
 
 from .models import Task
@@ -6,3 +8,6 @@ from .serializers import TaskSerializer
 class TaskViewSet(CommonBaseViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['id', 'name']
+    ordering = ['-id']
